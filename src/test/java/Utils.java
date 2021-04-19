cb52ca0914c328de9e315d4a1c7d6e43ccad3ad0
spiral99 <65442188+spiral99@users.noreply.github.com>
2021-04-19 23:52:19 +0300

    add chromedriver

--- src/test/java/Utils.java
+++ src/test/java/Utils.java
@@ -1,6 +1,8 @@
 // Configuration class for Form Automation project
 
 public class Utils {
+
     final static String BASE_URL = "https://formy-project.herokuapp.com/form";
     final static String CHROME_DRIVER_LOCATION = "chromedriver";
+
 }
